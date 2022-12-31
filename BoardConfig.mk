d6b3412187967eedf614593f34a031d944d5fc72
Albert I <kras@raphielgang.org>
2022-12-31 22:50:01 +0800

    ido: Include ntfs-3g in recovery build
    Allows mounting NTFS partitions in recovery.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,5 +87,6 @@ TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXTRA_LANGUAGES := true
+TW_INCLUDE_NTFS_3G := true
 TW_MAX_BRIGHTNESS := 255
 TW_THEME := portrait_hdpi
